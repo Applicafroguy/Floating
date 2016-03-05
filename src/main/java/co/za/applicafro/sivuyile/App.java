70db8f0f57d5ff4601bfd2065652d3c40e3fd280
Sivuyile Magutywa <211208086@mycput.ac.za>
2016-03-05 01:18:09 -0800

    working

---
+++ src/main/java/co/za/applicafro/sivuyile/App.java
@@ -0,0 +1,17 @@
+package co.za.applicafro.sivuyile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( add(1.2f,1.2f) );
+    }
+
+    public static float add(float a, float b){
+        return a+b;
+    }
+}
